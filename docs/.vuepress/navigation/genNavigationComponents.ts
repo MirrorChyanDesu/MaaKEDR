f33218768223e035298b2adfb04cfd26c6643444
APPLe-DF <abc15205483033@163.com>
2026-07-14 22:56:35 +0800

    feat: add QQ group quick join link to navbar and CONTACT file

--- docs/.vuepress/navigation/genNavigationComponents.ts
+++ docs/.vuepress/navigation/genNavigationComponents.ts
@@ -9,6 +9,8 @@ import { Locale } from './i18n.ts'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const QQ_LINK = 'https://qm.qq.com/q/clvWu1RoWI'
+
 interface MetaData {
   baseName: string
   order: number
@@ -177,8 +179,16 @@ export function genNavigationComponents(
 
   navigationComponentsWithOrder.sort((a, b) => a.order - b.order)
 
+  const navbar: ThemeNavItem[] = navigationComponentsWithOrder.map((i) => i.navItem)
+
+  navbar.push({
+    text: locale.name === 'zh' ? 'QQ群' : 'QQ Group',
+    icon: 'ri:qq-fill',
+    link: QQ_LINK,
+  })
+
   return {
-    navbar: navigationComponentsWithOrder.map((i) => i.navItem),
+    navbar,
     collections: navigationComponentsWithOrder.map((i) => i.collectionItem),
   }
 }
